35638931ed9850747229cd63ffd5fb4af13c9a22
Sergey Smalkov <36485527+SergeySmalkov@users.noreply.github.com>
2023-01-10 01:05:24 +0300

    Update Main.java

--- Future&Promise/Main.java
+++ Future&Promise/Main.java
@@ -50,5 +50,25 @@ public class Main {
         threadpool1.shutdown();
         threadpool2.shutdown();
     }
+//    Received news, 0 thread number: pool-1-thread-1 Time 2023-01-09T22:02:21.061919Z
+//    What is it?,   0 thread number: pool-2-thread-1 Time 2023-01-09T22:02:23.092356Z
+//    Received news, 1 thread number: pool-1-thread-1 Time 2023-01-09T22:02:26.091542Z
+//    What is it?,   1 thread number: pool-2-thread-1 Time 2023-01-09T22:02:28.092507Z
+//    Received news, 2 thread number: pool-1-thread-1 Time 2023-01-09T22:02:31.092437Z
+//    What is it?,   2 thread number: pool-2-thread-1 Time 2023-01-09T22:02:33.092837Z
+//    Received news, 3 thread number: pool-1-thread-1 Time 2023-01-09T22:02:36.092832Z
+//    What is it?,   3 thread number: pool-2-thread-1 Time 2023-01-09T22:02:38.093388Z
+//    Received news, 4 thread number: pool-1-thread-1 Time 2023-01-09T22:02:41.093231Z
+//    What is it?,   4 thread number: pool-2-thread-1 Time 2023-01-09T22:02:43.093603Z
+//    Received news, 5 thread number: pool-1-thread-1 Time 2023-01-09T22:02:46.093563Z
+//    What is it?,   5 thread number: pool-2-thread-1 Time 2023-01-09T22:02:48.094199Z
+//    Received news, 6 thread number: pool-1-thread-1 Time 2023-01-09T22:02:51.094036Z
+//    What is it?,   6 thread number: pool-2-thread-1 Time 2023-01-09T22:02:53.094642Z
+//    Received news, 7 thread number: pool-1-thread-1 Time 2023-01-09T22:02:56.094596Z
+//    What is it?,   7 thread number: pool-2-thread-1 Time 2023-01-09T22:02:58.094984Z
+//    Received news, 8 thread number: pool-1-thread-1 Time 2023-01-09T22:03:01.095Z
+//    What is it?,   8 thread number: pool-2-thread-1 Time 2023-01-09T22:03:03.095437Z
+//    Received news, 9 thread number: pool-1-thread-1 Time 2023-01-09T22:03:06.095402Z
+//    What is it?,   9 thread number: pool-2-thread-1 Time 2023-01-09T22:03:08.096544Z
 
 }
